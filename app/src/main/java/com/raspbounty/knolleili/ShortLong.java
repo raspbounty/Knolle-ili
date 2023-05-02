55ea4cf95d06ca64febcb2007e263b167d212fff
Luca Frenk <lf.aachen@gmail.com>
2023-05-02 14:30:17 +0200

    working

---
+++ app/src/main/java/com/raspbounty/knolleili/ShortLong.java
@@ -0,0 +1,10 @@
+package com.raspbounty.knolleili;
+
+class ShortLong {
+   public String shrt;
+   public String lng;
+   public ShortLong(String mShort, String mLong){
+      shrt = mShort;
+      lng = mLong;
+   }
+}
